d8142f16bb318c8ebb72f0413bb0f1eab0c03e8d
filauro1989 <filauro1989@gmail.com>
2022-02-23 15:06:05 +0100

    seeder compiled

--- database/migrations/2022_02_23_133424_create_comics_table.php
+++ database/migrations/2022_02_23_133424_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('thumb');
+            $table->float('price');
+            $table->string('series');
+            $table->date('sale_date')->nullable();
+            $table->string('type');
             $table->timestamps();
         });
     }
--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
+use App\Comic;
 
 class ComicSeeder extends Seeder
 {
@@ -9,8 +11,17 @@ class ComicSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 30; $i++) { 
+            $comic = new Comic();
+            $comic->title = $faker->word();
+            $comic->description = $faker->sentence(20, false);
+            $comic->thumb = $faker->imageUrl(300, 300, 'comics', true);
+            $comic->price = $faker->randomFloat();
+            $comic->series = $faker->word();
+            $comic->sale_date = $faker->dateTimeBetween();
+            $comic->type = $faker->word();
+        }
     }
 }
