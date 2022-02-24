fe733c46f8113c5ca75752736f1b0e70e93cd663
filauro1989 <filauro1989@gmail.com>
2022-02-24 15:47:18 +0100

    some style

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -12,25 +12,25 @@
         </div>
     </div>
 
-    <div class="container d-flex flex-wrap">
+    <div class="container d-flex flex-wrap justify-content-center">
         @foreach ($comic as $comics_item)
-            <div class="card w-25 d-flex align-items-center">
-                <div class="wrapper d-flex flex-column align-items-center">
+            <div class="card w-23 m-2 d-flex align-items-center rounded">
+                <div class="wrapper w-100 d-flex flex-column align-items-center ps-2 pe-2 text-center">
                     <div class="product d-flex justify-content-center">
-                        <a href="{{ route('comics.show', $comics_item)}}">
-                            <img
+                        <a class="w-100" href="{{ route('comics.show', $comics_item)}}">
+                            <img class="w-100 mt-2 rounded"
                                 src="{{$comics_item['thumb']}}" 
                                 alt="{{$comics_item['title']}}"
                             >
                         </a>
                     </div>
-                    <h2>{{ $comics_item['title'] }}</h2>
-                    <p class="text-center">{{ $comics_item['description'] }}</p>
+                    <h2 class="text-uppercase mt-5">{{ $comics_item['title'] }}</h2>
+                    <p>{{ $comics_item['description'] }}</p>
                     <h2>{{ $comics_item['price'] }}</h2>
                     <h3>{{ $comics_item['series'] }}</h3>
                     <h3>{{ $comics_item['sale_date'] }}</h3>
                     <h3>{{ $comics_item['type'] }}</h3>
-                    <a href="{{ route('comics.edit', $comics_item)}}" class="btn btn-primary">Edit</a>
+                    <a href="{{ route('comics.edit', $comics_item)}}" class="btn btn-primary mt-3 mb-3">Edit</a>
                 </div>
             </div>
         @endforeach
